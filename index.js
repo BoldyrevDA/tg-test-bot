1114c9d90faf3b54c934789fdf1ca0a075e6a11d
boldyrevda <22929109+BoldyrevDA@users.noreply.github.com>
2022-08-07 23:53:22 +0400

    update app

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ import 'dotenv/config'
 import express from 'express'
 import bodyParser from "body-parser";
 import axios from "axios";
+import { ocrSpace } from 'ocr-space-api-wrapper';
 
-const { TOKEN, SERVER_URL } = process.env;
+const { TOKEN, SERVER_URL, DEFAULT_IMAGE_ID } = process.env;
 
 const TELEGRAM_API = `https://api.telegram.org/bot${TOKEN}`;
+const TELEGRAM_FILE_API = `https://api.telegram.org/file/bot${TOKEN}`;
+
 const URI = `/webhook/${TOKEN}`; // TODO use X-Telegram-Bot-Api-Secret-Token
 const WEBHOOK_URL = SERVER_URL + URI;
 
@@ -17,29 +20,52 @@ const init = async () => {
     console.log(res.data);
 }
 
-app.post(URI, async (req, res) => {
-    console.log(req.body)
-    const { message } = req.body;
+async function noPhotoResponse(chatId) {
+    if (DEFAULT_IMAGE_ID) {
+        await axios.post(`${TELEGRAM_API}/sendPhoto`, {
+            chat_id: chatId,
+            photo: DEFAULT_IMAGE_ID
+        })
+    } else {
+        await axios.post(`${TELEGRAM_API}/sendMessage`, {
+            chat_id: chatId,
+            text: "photo required for OCR"
+        })
+    }
+}
 
+app.post(URI, async (req, res) => {
+    const { message } = req.body;
     const chatId = message.chat.id
-    const text = message.text
-    const fileId = message?.photo?.[2]?.file_id;
+    const photos = message?.photo;
+    const fileId = photos?.[photos.length - 1]?.file_id;
+
     console.log("fileId", fileId);
     if (fileId) {
         try {
             const response = await axios.post(`${TELEGRAM_API}/getFile`, { file_id: fileId });
-            const file = response?.result;
-            console.log("file.file_path", `${TELEGRAM_API}/${file?.file_path}`);
+            const file = response.data?.result;
+            const filePath = file?.file_path;
+            const fileUrl = `${TELEGRAM_FILE_API}/${filePath}`;
+
+            const ocrResponse = await ocrSpace(fileUrl, { filetype: filePath.slice(-3) });
+            const imgText = ocrResponse?.ParsedResults?.[0]?.ParsedText;
+
+            await axios.post(`${TELEGRAM_API}/sendMessage`, {
+                chat_id: chatId,
+                text: imgText || "I don't see text on picture"
+            })
+
+            console.log("file.file_path", `${TELEGRAM_FILE_API}/${file?.file_path}`);
         } catch (e) {
             console.log(e);
+            await axios.post(`${TELEGRAM_API}/sendMessage`, {
+                chat_id: chatId,
+                text: "server error"
+            })
         }
-    }
-
-    if (text) {
-        await axios.post(`${TELEGRAM_API}/sendMessage`, {
-            chat_id: chatId,
-            text: text
-        })
+    } else {
+        await noPhotoResponse(chatId)
     }
 
     return res.send()
