d69fe3ccd6a94da10e3af4e3150160fdfc91f6cf
Dmitrii Boldyrev <dmitrii_boldyrev@epam.com>
2023-11-29 01:00:49 +0400

    Update windows-service.js

--- windows-service.js
+++ windows-service.js
@@ -5,8 +5,11 @@ import path from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+const scriptPath = path.join(__dirname, 'index.js');
+console.log("=>(windows-service.js:11) scriptPath: ", scriptPath);
+
 export const winService = new Service({
     name: 'Telegram OCR Bot',
     description: 'Telegram OCR Bot as a Windows service',
-    script: path.join(__dirname,'index.js'),
+    script: scriptPath,
 });
