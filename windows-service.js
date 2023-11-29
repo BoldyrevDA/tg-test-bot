95fd3694d136062524626aedb2563a174c07b1ab
Dmitrii Boldyrev <dmitrii_boldyrev@epam.com>
2023-11-29 23:06:57 +0400

    Update windows-service.js

--- windows-service.js
+++ windows-service.js
@@ -8,6 +8,8 @@ const __dirname = path.dirname(__filename);
 const scriptPath = path.join(__dirname, 'index.js');
 const { TOKEN, WEBHOOK_HOST, DEFAULT_IMAGE_ID } = process.env;
 
+// TODO try to use "PM2" instead of "node-windows"
+
 console.log("=>(windows-service.js:11) scriptPath: ", scriptPath);
 
 export const winService = new Service({
